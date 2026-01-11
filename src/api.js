d05cefc58956335f065cce42d80b69b3bec06f02
rajankumarrkr <rajankumarrkr4576@gmail.com>
2026-01-11 20:51:00 +0530

    Add .env

--- src/api.js
+++ src/api.js
@@ -1,5 +1,4 @@
-const API_BASE = "http://localhost:5000/api";
-
+const API_BASE = import.meta.env.VITE_API_URL
 export async function apiGet(path) {
   const res = await fetch(`${API_BASE}${path}`);
   if (!res.ok) throw new Error("Request failed");
